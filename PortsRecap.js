2239b19a4b00bead0756df6d5e89351db1a9c054
aztayeb11 <aztayeb11@gmail.com>
2025-06-17 11:51:24 +0300

    Ports Files

---
+++ PortsRecap.js
@@ -0,0 +1,18 @@
+function retry(){
+    var retryBut = document.getElementById("retry");
+    window.location.href= 'file:///C:/Users/hussa/Desktop/MASTERM1ND/ports/Ports.html'
+    }
+    function cont(){
+    var continueBut = document.getElementById("continue");
+    window.location.href= 'file:///C:/Users/hussa/Desktop/MASTERM1ND/endCS/endCS.html'
+    }
+    window.onload = function() {
+        var y = document.getElementById("timeTaken2");
+        var totalTimeInSeconds2 = localStorage.getItem('totalTimeInSeconds2');
+        if (totalTimeInSeconds2 !== null) {
+            y.textContent = "Time taken: " + totalTimeInSeconds2;
+        } else {
+            y.textContent = "Time taken: Not available";
+        }
+    };
+    
